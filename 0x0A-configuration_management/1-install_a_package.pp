3f0a0a1f4688935b04a1e6240663df8181bd806c
NyatiZw <appsmaid19@gmail.com>
2023-06-23 05:17:36 -0700

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,7 +4,8 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-exec { 'install_flask':
+exec {'install_flask':
   command => 'pip3 install flask',
   path    => ['/usr/bin'],
+  unless  => 'pip3 show flask',
 }
