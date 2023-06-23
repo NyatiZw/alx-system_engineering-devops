e3a2da5352338d0135ab1a0bdea6255e6f58b2d2
NyatiZw <appsmaid19@gmail.com>
2023-06-23 04:10:41 -0700

    Install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# Puppet manifest to install a package flask from pip3
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => 'pip3 install flask',
+  unless  => 'pip3 show flask',
+}
