a272fc2dc0d375027936f1a1d94eef8ed19e7b10
NyatiZw <appsmaid19@gmail.com>
2023-07-24 16:35:05 -0700

    corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,45 @@
 #!/usr/bin/python3
 """Script that returns information from REST API"""
 
+
 if __name__ == "__main__":
     import os
-    import requests
+    from requests import get
+    from sys import argv, exit
 
-    def get_emp_todo(n):
-        """Function to get todo progress"""
-        base_url = 'https://jsonplaceholder.typicode.com/todos/1'
-        n = employee_id
-        n_det = "{}/employees/{}".format(base_url, n)
-        tasks_url = "{}/employees/{}/tasks".format(base_url, n)
+    def get_emp_todo(employee_id):
+        base_url = "https://jsonplaceholder.typicode.com"
+        employee_url = "{}/users?id=/{}".format(base_url) + employee_id
+        todo_url = "{}/todos?userId={}".format(base_url) + employee_id
 
-        # Fetch employee information
-        employee_response = requests.get(n_det)
-        employee_data = employee_response.json()
-        employee_name = employee_data.get('name', 'n')
+        employee_details = request.get(employee_url)
+        todo_details = request.get(todo_url)
 
-        # Fetch tasks information
-        tasks_response = requests.get(tasks_url)
-        tasks_data = tasks_response.json()
-        total_tasks = len(tasks_data)
-        completed_tasks = [task for task in tasks_data
-                           if task.get('completed', False)]
-        num_completed_tasks = len(completed_tasks)
+        try:
+            json_usr = employee_details.json()
+            json_todo = todo_details.json()
+        except ValueError:
+            print("Not a Json file")
 
-        """Display the progress"""
-        print("Employee {} is done with tasks({}/{}):"
-              .format(employee_name, num_completed_tasks, total_tasks))
-        for task in completed_tasks:
-            print("\t{}".format(task['title']))
+        if json_usr and json_todo:
+            name = json_usr[0].get('name')
+            total_tasks = len(json_todo)
+            tasks_done = sum(task.get("completed")
+                            for task in json_todo if task)
+
+            print("Employee {} is done with taskss({}/{}):".format(name, tasks_done, total_tasks))
+
+            for todo in json_todo:
+                task_title = todo.get('title')
+                if todo.get("completed"):
+                    print("\t {}".format(task_title))
+
+    if len(argv) != 2:
+        print("Usage: python script_name.py <employee_id>")
+        exit(1)
+
+    try:
+        employee_id = int(argv[1])
+    except ValueError:
+        print("Invalid ID")
+        exit(1)
