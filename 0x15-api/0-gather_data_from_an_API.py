0c90f239605f8c432e2d704cd0e0432e29a762d8
NyatiZw <appsmaid19@gmail.com>
2023-07-24 14:52:35 -0700

    fixed libraries order

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,17 +5,18 @@ import os
 import requests
 
 
-def get_emp_todo(employee_id):
-    """Function to get todo progress"""
-    base_url = "https://jsonplaceholder.typicode.com/todos/1"
-    employee_url = "{}/employees/{}".format(base_url, employee_id)
-    tasks_url = "{}/employees/{}/tasks".format(base_url, employee_id)
+if __name__ == "__main__":
 
-    try:
-        # Fetch empployee information
+    def get_emp_todo(employee_id):
+        """Function to get todo progress"""
+        base_url = "https://jsonplaceholder.typicode.com/todos/1"
+        employee_url = "{}/employees/{}".format(base_url, employee_id)
+        tasks_url = "{}/employees/{}/tasks".format(base_url, employee_id)
+
+        # Fetch employee information
         employee_response = requests.get(employee_url)
         employee_data = employee_response.json()
-        employee_name = employee_data.get('name', 'Unknown Employee')
+        employee_name = employee_data.get('name', 'employee_id')
 
         # Fetch tasks information
         tasks_response = requests.get(tasks_url)
@@ -31,10 +32,3 @@ def get_emp_todo(employee_id):
         for task in completed_tasks:
             print("\t{}".format(task['title']))
 
-    except requests.exceptions.RequestException as e:
-        print("Error connecting to the API:", e)
-
-
-if __name__ == "__main__":
-    employee_id = int(input("Enter the Employee ID: "))
-    get_emp_todo(employee_id)
