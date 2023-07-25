ba4bdbcbbfdea0ad544f6d35ce52d9d7099ccb04
NyatiZw <appsmaid19@gmail.com>
2023-07-24 18:23:21 -0700

    corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,18 +6,18 @@ if __name__ == "__main__":
     from requests import get
     from sys import argv, exit
 
+    emp_id = argv[1]
     base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = "{}/users?id=/{}".format(base_url) + employee_id
-    todo_url = "{}/todos?userId={}".format(base_url) + employee_id
+    employee_url = "{}/users?id=/{}".format(base_url) + emp_id
+    todo_url = "{}/todos?userId={}".format(base_url) + emp_id
 
-    employee_details = request.get(employee_url)
-    todo_details = request.get(todo_url)
+    employee_details = requests.get(employee_url)
+    todo_details = requests.get(todo_url)
 
     try:
         json_usr = employee_details.json()
         json_todo = todo_details.json()
     except ValueError:
-        print("Not a Json file")
 
     if json_usr and json_todo:
         name = json_usr[0].get('name')
@@ -25,15 +25,10 @@ if __name__ == "__main__":
         tasks_done = sum(task.get("completed")
                          for task in json_todo if task)
 
-        print("Employee {} is done with taskss({}/{}):".format(name,
+        print("Employee {} is done with tasks({}/{}):".format(name,
               tasks_done, total_tasks))
 
         for todo in json_todo:
             task_title = todo.get('title')
             if todo.get("completed"):
                 print("\t {}".format(task_title))
-
-    try:
-        employee_id = int(argv[1])
-    except ValueError:
-        exit(1)
