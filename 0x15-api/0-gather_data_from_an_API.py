c17e1ad5e61da03866ecf16c6c5feb10387e63d6
NyatiZw <appsmaid19@gmail.com>
2023-07-26 14:54:38 -0700

    corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
     try:
         id = argv[1]
         is_int = int(id)
+    except ValueError:
+        exit()
 
     url_user = "https://jsonplaceholder.typicode.com/users?id=" + id
     url_todo = "https://jsonplaceholder.typicode.com/todos?userId=" + id
@@ -18,6 +20,8 @@ if __name__ == "__main__":
     try:
         js_user = r_user.json()
         js_todo = r_todo.json()
+    except ValueError:
+        exit()
 
     if js_user and js_todo:
         EMPLOYEE_NAME = js_user[0].get('name')
