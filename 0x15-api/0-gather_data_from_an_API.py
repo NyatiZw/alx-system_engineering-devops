c6059e7178cca51cdbdc35a3e575b9e46710a130
NyatiZw <appsmaid19@gmail.com>
2023-07-26 14:44:45 -0700

    corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Script that returns information from REST API
-"""
-
+""" Script that returns information from REST API """
 
 if __name__ == "__main__":
     from requests import get
@@ -14,28 +11,22 @@ if __name__ == "__main__":
     except BaseException:
         exit()
 
-    # API endpoints for the user and todo details based on the provided user
-    # ID.
     url_user = "https://jsonplaceholder.typicode.com/users?id=" + id
     url_todo = "https://jsonplaceholder.typicode.com/todos?userId=" + id
 
-    # Send GET request to the API endpoints.
     r_user = get(url_user)
     r_todo = get(url_todo)
 
     try:
-        # Parse the JSON response data.
         js_user = r_user.json()
         js_todo = r_todo.json()
 
     if js_user and js_todo:
-        # Extract relevant information from the JSON data.
         EMPLOYEE_NAME = js_user[0].get('name')
         TOTAL_NUMBER_OF_TASKS = len(js_todo)
         NUMBER_OF_DONE_TASKS = sum(item.get("completed")
                                    for item in js_todo if item)
 
-        # Print the retrieved information in a formatted manner.
         print("Employee {} is done with tasks({}/{}):"
               .format(EMPLOYEE_NAME,
                       NUMBER_OF_DONE_TASKS,
