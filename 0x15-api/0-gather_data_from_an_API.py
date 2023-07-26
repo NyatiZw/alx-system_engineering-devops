9aa00e2aadccf738e442a2cac8877eb2fbdf86f8
NyatiZw <appsmaid19@gmail.com>
2023-07-26 14:03:29 -0700

    corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     except:
         exit()
 
-    url_user = "htpps://jsonplaceholder.typicode.com/users?id=" + id
+    url_user = "https://jsonplaceholder.typicode.com/users?id=" + id
     url_todo = "https://jsonplaceholder.typicode.com/todos?userId=" + id
 
     r_user = get(url_user)
@@ -33,8 +33,10 @@ if __name__ == "__main__":
                                    for item in js_todo if item)
 
         print("Employee {} is done with tasks({}/{}):"
-                .format(EMPLOYEE_NAME,
-                        NUMBER_OF_DONE_TASKS,
-                        TOTAL_NUMBEROF_TASKS))
+              .format(EMPLOYEE_NAME,
+                      NUMBER_OF_DONE_TASKS,
+                      TOTAL_NUMBEROF_TASKS))
         for todo in js_todo:
             TASK_TITLE = todo.get('title')
+            if todo.get("completed"):
+                print("\t {}".format(TASK_TITLE))
