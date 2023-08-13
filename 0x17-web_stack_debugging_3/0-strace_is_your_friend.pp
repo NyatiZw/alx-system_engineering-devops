2a86edf872926865f19f6728783af35989c7fdfe
NyatiZw <appsmaid19@gmail.com>
2023-08-13 23:11:25 +0000

    corrections

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,14 @@
 #Puppet manifest to ammend server
+
 file { '/var/www/html/index.html':
-  ensure => file,
-  mode   => '0644',
-  owner  => 'www-data',
-  group  => 'www-data',
+  ensure  => file,
+  mode    => '0644',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => '<title>Holberton &#8211; Just another WordPress site</title>
+<link rel="alternate" type="application/rss+xml" title="Holberton &raqou; Feed" href="http://127.0.0.1/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="Holberton &raqou; Comments Feed" href="http://127.0.0.1/?feed=comments-rss2" />
+<div id="wp-custom-header" class="wp-custom-header"><img src="http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg" width="2000" height="1200" alt="Holberton" /></div> </div>
+<h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
+<p>Yet another bug by a HOlberton student</p>',
 }
