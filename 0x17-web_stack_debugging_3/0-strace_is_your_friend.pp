e33d2bc34e47c1cc044ce3f232627dd239f2616c
NyatiZw <appsmaid19@gmail.com>
2023-08-13 22:30:00 +0000

    second commit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 #Puppet manifest to ammend server
 file { '/var/www/html/index.html':
   ensure => file,
-  mode => '0644',
-  owner => 'www-data',
-  group => 'www-data',
+  mode   => '0644',
+  owner  => 'www-data',
+  group  => 'www-data',
 }
