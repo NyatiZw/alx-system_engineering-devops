5b16e90c9a68432255dfb889efe26bd005c10028
NyatiZw <appsmaid19@gmail.com>
2023-08-14 00:48:06 +0000

    cleanup

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,12 @@
 #Puppet manifest to ammend server
-
 file { '/var/www/html/index.html':
+  ensure  => file,
+  mode    => '0644',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
+
+file { '/var/www/html/Holberton':
   ensure  => file,
   mode    => '0644',
   owner   => 'www-data',
